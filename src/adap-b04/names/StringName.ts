e2dd31a13f25315cdc672f7f057059e6516a1738
Roman Graber <roman.graber@fau.de>
2025-11-28 18:11:14 +0100

    update stuff

--- src/adap-b04/names/StringName.ts
+++ src/adap-b04/names/StringName.ts
@@ -1,6 +1,10 @@
 import { DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "../common/Printable";
 import { Name } from "./Name";
 import { AbstractName } from "./AbstractName";
+import {StringArrayName} from "../../adap-b02/names/StringArrayName";
+import {IllegalArgumentException} from "../common/IllegalArgumentException";
+import {InvalidStateException} from "../common/InvalidStateException";
+import {MethodFailedException} from "../common/MethodFailedException";
 
 export class StringName extends AbstractName {
 
@@ -8,64 +12,121 @@ export class StringName extends AbstractName {
     protected noComponents: number = 0;
 
     constructor(source: string, delimiter?: string) {
-        super();
-        throw new Error("needs implementation or deletion");
+        super(delimiter);
+        this.name = source;
+
+        this.noComponents = this.getNames().length;
     }
 
-    public clone(): Name {
-        throw new Error("needs implementation or deletion");
+    private convertBackToString(san: StringArrayName): string {
+        let str = ""
+        for(let i = 0; i < san.getNoComponents(); i++) {
+            str += san.getComponent(i) + this.delimiter;
+        }
+        return str.substring(0, str.length - 1);
     }
 
-    public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
-    }
+    private getNames(): string[] {
+        let current = "";
+        let names: string[] = [];
 
-    public asDataString(): string {
-        throw new Error("needs implementation or deletion");
-    }
+        for (let i = 0; i < this.name.length; i++) {
+            const char = this.name[i];
 
-    public isEqual(other: Name): boolean {
-        throw new Error("needs implementation or deletion");
-    }
+            if (char === this.delimiter) {
+                // Count backslashes before the delim
+                let backslashCount = 0;
+                for (let j = i - 1; j >= 0 && this.name[j] === ESCAPE_CHARACTER; j--) {
+                    backslashCount++;
+                }
 
-    public getHashCode(): number {
-        throw new Error("needs implementation or deletion");
-    }
+                // If not escaped, split here
+                if (backslashCount % 2 === 0) {
+                    names.push(current);
+                    current = "";
+                    continue;
+                }
+            }
 
-    public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
+            current += char;
+        }
+        names.push(current);
+        return names;
     }
 
     public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+        const noComponents = this.noComponents;
+        InvalidStateException.assert(noComponents >= 0);
+        return this.noComponents;
     }
 
     public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+        IllegalArgumentException.assert(this.isValidIndex(i));
+        return (new StringArrayName(this.getNames(), this.delimiter)).getComponent(i);
     }
 
     public setComponent(i: number, c: string) {
-        throw new Error("needs implementation or deletion");
+        IllegalArgumentException.assert(this.isValidIndex(i));
+        IllegalArgumentException.assert(this.isValidComponent(c));
+
+        let sac = new StringArrayName(this.getNames(), this.delimiter)
+        sac.setComponent(i, c);
+        this.name = this.convertBackToString(sac);
+
+        const newComponent: string = this.getComponent(i);
+
+        InvalidStateException.assert(this.isValidComponent(newComponent));
+
+        MethodFailedException.assert(this.getComponent(i) === c);
     }
 
     public insert(i: number, c: string) {
-        throw new Error("needs implementation or deletion");
+        IllegalArgumentException.assert(this.isValidIndex(i));
+        IllegalArgumentException.assert(this.isValidComponent(c));
+
+        const oldLength = this.getNoComponents();
+
+        let sac = new StringArrayName(this.getNames(), this.delimiter)
+        sac.insert(i, c)
+        this.name = this.convertBackToString(sac);
+        this.noComponents++;
+
+        const newLength: number = this.getNoComponents();
+        const newComponent: string = this.getComponent(i);
+
+        InvalidStateException.assert(this.isValidComponent(newComponent));
+
+        MethodFailedException.assert(newLength === oldLength + 1 && newComponent === c);
     }
 
     public append(c: string) {
-        throw new Error("needs implementation or deletion");
+        IllegalArgumentException.assert(this.isValidComponent(c));
+
+        const oldLength = this.getNoComponents();
+
+        let sac = new StringArrayName(this.getNames(), this.delimiter)
+        sac.append(c)
+        this.name = this.convertBackToString(sac);
+        this.noComponents++;
+
+        const newLength: number = this.getNoComponents();
+        const newComponent: string = this.getComponent(newLength - 1);
+
+        InvalidStateException.assert(this.isValidComponent(newComponent));
+
+        MethodFailedException.assert(newLength === oldLength + 1 && newComponent === c);
     }
 
     public remove(i: number) {
-        throw new Error("needs implementation or deletion");
-    }
 
-    public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
-    }
+        IllegalArgumentException.assert(this.isValidIndex(i));
+        const oldLength = this.getNoComponents();
 
+        let sac = new StringArrayName(this.getNames(), this.delimiter)
+        sac.remove(i)
+        this.name = this.convertBackToString(sac);
+        this.noComponents--;
+
+        MethodFailedException.assert(this.getNoComponents() === oldLength);
+    }
 }
